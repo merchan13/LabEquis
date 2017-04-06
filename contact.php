406d165bcd2883c75ae131eb86bcd675a72f2933
Daniel Da Mata De Gouveia <dnai7.da.mata@gmail.com>
2017-04-05 23:27:11 -0400

    Envio email funcionado

--- contact.php
+++ contact.php
@@ -1,21 +1,23 @@
 <?php
-$field_name = $_POST['cf_name'];
-$field_email = $_POST['cf_email'];
-$field_message = $_POST['cf_message'];
+$field_name = $_POST['name'];
+$field_email = $_POST['email'];
+$field_message = $_POST['message'];
 
-$mail_to = 'labequis@labequis.com';
-$subject = 'Message from a site visitor '.$field_name;
 
-$body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$field_email."\n";
-$body_message .= 'Message: '.$field_message;
+$subject = "Message from a site visitor ".$field_name;
 
-$headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
+$body_message = "From: ".$field_name."\r\n";
+$body_message .= "E-mail: ".$field_email."\r\n";
+$body_message .= "Message: ".$field_message;
 
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
 
-if ($mail_status) { ?>
+
+$headers = "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+$headers .= "From: ".$field_email."\r\n";
+$headers .= "Reply-To: ".$field_email."\r\n";
+
+if (mail("jmerchan@labequis.com", $subject, $body_message, $headers)) { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Thank you for the message. We will contact you shortly.');
 		window.location = 'index.html';
