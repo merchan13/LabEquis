ff97ff2c85b8abe52c3a3d7618bede7650fc8541
jmerchan <merchan1395@gmail.com>
2017-04-12 13:01:53 -0400

    A la mierda lo viejo

--- js/header.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function () {
-    if ($(window).scrollTop() > $('#fixed-navbar').offset().top && !($('#fixed-navbar').hasClass('posi'))) {
-        $('#fixed-navbar').addClass('posi');
-    } else if ($(window).scrollTop() <= 768) {
-        $('#fixed-navbar').removeClass('posi');
-    }
-});
